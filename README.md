e78277c0802f2b44dfab5e2bbb72aed8e4841d83
Jorge R. <jorge@programmer.net>
2025-02-18 11:03:35 -0700

    Add CSS & JS

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-<h4 align="center">
-:construction: Work in Progress :construction:
-</h4>
+# Custom Dropdown Menu 🖱️
+
+A customizable dropdown menu built with vanilla HTML, CSS, and JavaScript.
+
+![Dropdown Preview](https://github.com/user-attachments/assets/fbe29a7e-acf9-427b-9aaa-f4fd05536182)
+
+---
+
+## Features ✨  
+- ✅ **Three visual states**:  
+  - **Default**: Placeholder text  
+  - **Open**: Visible options list  
+  - **Selected**: Displays the chosen item  
+- 🖍️ Selected item highlighting  
+- 🚀 Auto-close when clicking outside  
+- ⚡ No external dependencies  
+
+---
+
+## Installation 📦  
+1. Open your terminal and Clone the repository:  
+   ```bash  
+   git clone git@github.com:neolinkDev/custom_dropdown.git
+
+2. Open index.html in your browser. 
+
+Optionally you can install the VSC Live Server extension, launch the server and open it in your browser.
--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 
 
-console.log('Hello there!');
+const $dropdown = document.querySelector('.dropdown');
+
+const $trigger = $dropdown.querySelector('.dropdown__trigger');
+const $selectedValueDisplay = $dropdown.querySelector('.dropdown__value');
+const $list = $dropdown.querySelector('.dropdown__list');
+const $arrow = $dropdown.querySelector('.arrow');
+
+const $items = $dropdown.querySelectorAll('.dropdown__item');
+
+// toggle dropdown
+$trigger.addEventListener('click', () => {
+  $trigger.classList.toggle('dropdown__trigger--open');
+  $arrow.classList.toggle('arrow--open');
+  $list.classList.toggle('dropdown__list--open');
+})
+
+// close by clicking outside
+document.addEventListener('click', (e) => {
+  if (!$dropdown.contains(e.target)) {
+    $trigger.classList.remove('dropdown__trigger--open');
+    $arrow.classList.remove('arrow--open');
+    $list.classList.remove('dropdown__list--open');
+  }
+});
+
+// item selection
+$items.forEach(item => {
+
+  item.addEventListener('click', () => {
+
+      // remove previous selection
+      $items.forEach(i => i.classList.remove('dropdown__item--selected'));
+      
+      // update display
+      item.classList.add('dropdown__item--selected');
+      $selectedValueDisplay.textContent = item.textContent;
+      
+      // close dropdown
+      $list.classList.remove('dropdown__list--open');
+      $trigger.classList.remove('dropdown__trigger--open');
+      $arrow.classList.remove('arrow--open');
+  });
+});
